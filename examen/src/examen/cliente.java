e3f53fbd788b564c1c49fa53cdb9f8827df38a2a
Sebashj <sebasamodeluniverso@gmail.com>
2022-10-24 22:18:45 -0500

    ec

---
+++ examen/src/examen/cliente.java
@@ -0,0 +1,41 @@
+package examen;
+import java.util.ArrayList;
+
+public class cliente {
+String nombre;
+String telefono;
+String direccion;
+ArrayList<cuenta> miscuentas;
+
+public cliente() {
+	miscuentas=new ArrayList<cuenta>();
+}
+public void addCuenta(cuenta c) {
+	miscuentas.add(c);
+}
+public ArrayList<cuenta> getMiscuentas(){
+	return miscuentas;
+}
+public cliente(String nombre, String telefono, String direccion) {
+	super();
+}
+public String getNombre() {
+	return nombre;
+}
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+public String getTelefono() {
+	return telefono;
+}
+public void setTelefono(String telefono) {
+	this.telefono = telefono;
+}
+public String getDireccion() {
+	return direccion;
+}
+public void setDireccion(String direccion) {
+	direccion = direccion;
+}
+
+}
